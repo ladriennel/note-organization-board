9bb0e7e97aa6a00c625197939a62f1b4cfe0a0f3
Adrienne <adriennel034@gmail.com>
2025-01-23 21:20:15 -0500

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
@@ -8,6 +9,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    credentials: true  
+  }))
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
